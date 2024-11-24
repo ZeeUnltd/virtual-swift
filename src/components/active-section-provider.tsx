7e5e3ca0d1089f76c9c40ab8af680608d8f98f7a
Azeez Gafar <azegaf@gmail.com>
2024-11-24 20:06:59 +0100

    Deployment fix

--- src/components/active-section-provider.tsx
+++ src/components/active-section-provider.tsx
@@ -60,7 +60,6 @@ export const ActiveSectionProvider = ({ children }: PropsWithChildren) => {
         createRowRecord,
         updateRowRecord,
         deleteRowRecord,
-        setActiveSection
       }}
     >
       {children}
